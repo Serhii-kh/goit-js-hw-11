149311c1a049e73e0ef91abf35955e004e8ad9d7
Serhii Naboka <drifterzzz@gmail.com>
2023-03-28 17:45:43 +0300

    start -3

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const formRef = document.querySelector('#search-form');
+const inputRef = document.querySelector('searchQuery');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+console.log(formRef);
+console.log(formRef.elements.searchQuery);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(e.target.elements.searchQuery.value);
+}
