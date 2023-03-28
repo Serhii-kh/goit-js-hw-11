e659a8c85a1add91528c6c20e30655e13c58f039
Serhii-kh <114227063+Serhii-kh@users.noreply.github.com>
2023-03-28 14:46:05 +0000

    Deploying to gh-pages from @ Serhii-kh/goit-js-hw-11@149311c1a049e73e0ef91abf35955e004e8ad9d7 🚀

---
+++ index.1144243f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#search-form");document.querySelector("searchQuery");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.elements.searchQuery.value)})),console.log(e),console.log(e.elements.searchQuery);
+//# sourceMappingURL=index.1144243f.js.map
---
+++ index.de28e82c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-form");document.querySelector("searchQuery");e.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.elements.searchQuery.value)})),console.log(e),console.log(e.elements.searchQuery)}();
+//# sourceMappingURL=index.de28e82c.js.map
