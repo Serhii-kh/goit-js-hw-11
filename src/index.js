77f89aa495d433ae17866bc35b957b6224a53ea8
Serhii Naboka <drifterzzz@gmail.com>
2023-03-31 21:09:13 +0300

    scsc

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('#search-form');
+const loadMoreBtnRef = document.querySelector('load-more');
 const API_KEY = '34855628-78991e6cca5fe0310616aeb58';
 const BASE_URL = 'https://pixabay.com/api/';
 const BASE_FETCH_OPTIONS =
-  'image_type=photo&orientation=horizontal&safesearch=true';
+  'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 
 const instance = axios.create({
   baseURL: BASE_URL,
 });
 
 formRef.addEventListener('submit', onFormSubmit);
+// loadMoreBtnRef.addEventListener('click', onLoadMoreBtnClick);
 
 async function onFormSubmit(e) {
   e.preventDefault();
 
   const inputRefValue = e.currentTarget.elements.searchQuery.value;
+  // const submitBtnRef = e.currentTarget.elements.;
+  let page = 2;
   const fetchImages = async () => {
     return await instance.get(
-      `?key=${API_KEY}&q=${inputRefValue}&${BASE_FETCH_OPTIONS}`
+      `?key=${API_KEY}&q=${inputRefValue}&${BASE_FETCH_OPTIONS}&page=${page}`
     );
   };
 
@@ -30,15 +34,20 @@ async function onFormSubmit(e) {
     .then(({ data }) => {
       if (data.hits.length === 0) {
         Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
+          'Sorry, there are no images matching your search query. Please try again.'
         );
         return;
       }
+      console.log(data.hits);
+
       return data.hits;
     })
     .then(renderImgCards)
     .catch(error => console.log(error));
+  page += 1;
 }
+
+function onLoadMoreBtnClick() {}
 
 function renderImgCards(images) {
   const markup = images
